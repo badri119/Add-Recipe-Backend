65466613149c64b71b2027881313610a4de8a2d0
badri119 <badrinathv98@gmail.com>
2023-11-29 09:25:28 -0800

    signup navigation to login after user creates an account successfully

--- Routes/signup.js
+++ Routes/signup.js
@@ -23,7 +23,7 @@ router.post("/", async (req, res) => {
 
   try {
     const newUser = await user.save();
-    res.status(200).json({ message: "User Created Successfully" });
+    res.status(200).json({ message: "User created successfully" });
   } catch (err) {
     res.status(500).json({ message: "Internal Server Error" });
   }
